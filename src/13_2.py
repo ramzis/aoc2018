d330f3864852d6e697d71cf6ffb3cf2a472bfcea
Tadas Audinis <18498995+ramzis@users.noreply.github.com>
2018-12-13 22:33:42 -0800

    Day 13

---
+++ src/13_2.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+
+class Task:
+
+    input_dir = '../input'
+    output_dir = '../output'
+
+    def __init__(self, day):
+        self.day = day
+        self.input = '{}/{}.txt'.format(self.input_dir, day)
+
+    def write_solution(self):
+        f = open('{}/{}.txt'.format(self.output_dir, self.day),
+                 'wt', encoding='utf-8')
+        f.write(str(self.solution) + '\n')
+
+    def solve(self):
+        self.solution = ()
+        layout = []
+        with open(self.input) as f:
+            for line in f:
+                layout.append(self.parse_line(line))
+
+        carts = []
+
+        ys = len(layout)
+        xs = len(layout[0])
+
+        for y in range(ys):
+            for x in range(xs):
+                item = layout[y][x]
+                if item in ['<', '>']:
+                    layout[y][x] = '-'
+                    carts.append(((x, y), item, 0))
+                elif item in ['^', 'v']:
+                    layout[y][x] = '|'
+                    carts.append(((x, y), item, 0))
+
+        tick = 0
+        collision = False
+        while len(carts) > 1:
+            tick += 1
+            for c in range(len(carts)):
+                if carts[c] == ():
+                    continue
+                curr_x, curr_y = carts[c][0]
+                next_x = next_y = 0
+                sign_curr = carts[c][1]
+                if sign_curr == '^':
+                    next_x, next_y = curr_x, curr_y - 1
+                elif sign_curr == '<':
+                    next_x, next_y = curr_x - 1, curr_y
+                elif sign_curr == '>':
+                    next_x, next_y = curr_x + 1, curr_y
+                elif sign_curr == 'v':
+                    next_x, next_y = curr_x, curr_y + 1
+
+                if (next_x, next_y) in [cart[0] for cart in carts if cart != ()]:
+                    carts = list(map(
+                        lambda x: () if (x == carts[c] or x[0] == (next_x, next_y)) else x, carts))
+                    continue
+
+                sign_target = layout[next_y][next_x]
+
+                if sign_target in ['-', '|']:
+                    carts[c] = ((next_x, next_y), sign_curr, carts[c][2])
+                elif sign_target == '/':
+                    next_sign = \
+                        '>' if sign_curr == '^' else \
+                        'v' if sign_curr == '<' else \
+                        '^' if sign_curr == '>' else \
+                        '<' if sign_curr == 'v' else ''
+
+                    carts[c] = \
+                        ((next_x, next_y), next_sign, carts[c][2])
+                elif sign_target == '\\':
+                    next_sign = \
+                        '<' if sign_curr == '^' else \
+                        '^' if sign_curr == '<' else \
+                        'v' if sign_curr == '>' else \
+                        '>' if sign_curr == 'v' else ''
+
+                    carts[c] = \
+                        ((next_x, next_y), next_sign, carts[c][2])
+                elif sign_target == '+':
+                    next_sign = \
+                        ['<', '^', '>'][carts[c][2]] if sign_curr == '^' else \
+                        ['v', '<', '^'][carts[c][2]] if sign_curr == '<' else \
+                        ['^', '>', 'v'][carts[c][2]] if sign_curr == '>' else \
+                        ['>', 'v', '<'][carts[c][2]] if sign_curr == 'v' else ''
+
+                    carts[c] = \
+                        ((next_x, next_y), next_sign, (carts[c][2] + 1) % 3)
+
+            carts = sorted([c for c in carts if c != ()],
+                           key=lambda x: (x[0][1], x[0][0]))
+
+        self.solution = "{},{}".format(carts[0][0][0], carts[0][0][1])
+
+    def parse_line(self, line):
+        return list(line.rstrip('\n'))
+
+
+if __name__ == '__main__':
+    task = Task('13_2')
+    task.solve()
+    task.write_solution()
